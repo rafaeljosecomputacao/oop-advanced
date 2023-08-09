0ccc601e8c0dcef9deffedf32943bedbf7041768
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2023-08-09 12:11:22 -0300

    files

---
+++ CyberStore/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace CyberStore.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double TotalPrice()
+        {
+            return Price * Quantity;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ","
+                + TotalPrice().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- CyberStore/Program.cs
+++ CyberStore/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Globalization;
+using CyberStore.Entities;
+
+namespace CyberStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            string sourcePath = @"c:\files\items.csv";
+            
+            try
+            {
+                using (StreamReader sr = File.OpenText(sourcePath))
+                {           
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        double price = double.Parse(line[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(line[2]);
+                        products.Add(new Product(name, price, quantity));
+                    }
+                }
+
+                Directory.CreateDirectory(@"c:\files\out");
+
+                string targetPath = @"c:\files\out\summary.csv";
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (Product product in products)
+                    {
+                        sw.WriteLine(product);
+                    }
+                }
+
+                using (StreamReader sr = File.OpenText(targetPath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
