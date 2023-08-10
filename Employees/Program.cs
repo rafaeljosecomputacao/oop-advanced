965ac42e4c9951bb3e081f5fab800651cdb92f43
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2023-08-10 11:03:34 -0300

    employees-interface-icomparable

---
+++ Employees/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Employees.Entities;
+
+namespace Employees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\files\items.csv";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> employees = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        employees.Add(new Employee(sr.ReadLine()));
+                    }
+                    employees.Sort();
+                    foreach (Employee employee in employees)
+                    {
+                        Console.WriteLine(employee);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
