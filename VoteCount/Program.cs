3ed5c9fee5189eb844780f9b748796cc06181cb6
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2023-08-13 14:13:28 -0300

    generics-set-dictionary

---
+++ VoteCount/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace VoteCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string candidate = line[0];
+                        int votes = int.Parse(line[1]);
+
+                        if (dictionary.ContainsKey(candidate))
+                        {
+                            dictionary[candidate] += votes;
+                        }
+                        else
+                        {
+                            dictionary[candidate] = votes;
+                        }
+                    }
+                }
+                
+                foreach (KeyValuePair<string, int> item in dictionary)
+                {
+                    Console.WriteLine(item.Key + ": " + item.Value);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
