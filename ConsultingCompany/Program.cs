e0d23b3e5e5e07ed1f99e2a45c5216b42a1e43e9
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2023-08-13 12:41:46 -0300

    consulting-company-generics

---
+++ ConsultingCompany/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using ConsultingCompany.Entities;
+using ConsultingCompany.Services;
+
+namespace ConsultingCompany
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                products.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Console.WriteLine("Most expensive:");
+            Console.WriteLine(calculationService.Max(products));
+        }
+    }
+}
