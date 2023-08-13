ac0b325374d501ebfad2604e7f4aaf62e9a32558
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2023-08-13 12:08:57 -0300

    printer-generics

---
+++ Printer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Printer.Services;
+
+namespace Printer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<int> printService = new PrintService<int>();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+
+            Console.Write("First: " + printService.First());
+        }
+    }
+}
