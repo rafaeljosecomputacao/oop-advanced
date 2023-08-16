6c339e1623f278bc014e86541b1be62e32583fc2
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2023-08-16 20:21:43 -0300

    extensions-methods

---
+++ PostTime/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PostTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2023, 08, 16, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
---
+++ PostView/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PostView
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
